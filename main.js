63b9f229db0800bc98c587b81d67c05d44bf1abc
hyunjujujuju <hyunjujujuju@gmail.com>
2026-03-01 06:18:58 +0000

    Upload current code for product builder lecture

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Add JS here
+const generatorButton = document.getElementById('generator-button');
+const lottoNumbersContainer = document.getElementById('lotto-numbers');
+
+generatorButton.addEventListener('click', () => {
+  lottoNumbersContainer.innerHTML = '';
+  const numbers = new Set();
+  while (numbers.size < 6) {
+    const randomNumber = Math.floor(Math.random() * 45) + 1;
+    numbers.add(randomNumber);
+  }
+
+  for (const number of numbers) {
+    const numberElement = document.createElement('div');
+    numberElement.classList.add('lotto-number');
+    numberElement.textContent = number;
+    lottoNumbersContainer.appendChild(numberElement);
+  }
+});
